0522c85c8ef3e805e72bda7c4e9313e3113bd3e3
VENKATADINESHPOSA <venkatadineshposa@gmail.com>
2024-02-20 22:44:59 +0530

    added routing to the app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
-import Directory from "./components/directory/directory.component";
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
 const App = () => {
-  return <Directory />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
+};
+
+const Shop = () => {
+  return (
+    <div>
+      <h1>This is Shop Page</h1>
+    </div>
+  );
 };
 
 export default App;
